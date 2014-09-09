dcf5c5218493684bda88f86d332a5e4b9d3ff232
yanagi_kenji <yanagi_kenji@griphone.co.jp>
2014-09-09 14:46:14 +0900

    Connect testing mysql-server

--- html/index.php
+++ html/index.php
@@ -1,15 +1,37 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/xhprof.php';
+require 'vendor/autoload.php';
+require 'xhprof.php';
+error_reporting(E_ALL);
 
+// use Eloquent ORM
+use Illuminate\Database\Capsule\Manager as Capsule;  
+use Illuminate\Database\Schema\Blueprint as Schema;  
+ 
+// create model for Eloquent ORM mapped to REST API resource
+class Product extends Illuminate\Database\Eloquent\Model {
+  public $timestamps = false;
+}
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'test',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => ''
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 $app = new Bullet\App();
-$app->path('v1', function ($request) use ($app) {
-
-    $app->path('peopleApi', function ($request) use ($app) {
+$app->path('v1', function ($req) use ($app) {
+    $app->path('peopleApi/:id', function () use ($app) {
         $app->get(function() use ($app) {
-            return '[json-data]';
+            $products = Product::all();
+            return $products->toArray();
         });
         $app->post(function() use ($app) {
             return '[json-data]';
@@ -21,7 +43,6 @@ $app->path('v1', function ($request) use ($app) {
             return '[json-data]';
         });
     });
-
 });
 
 echo $app->run(new Bullet\Request());
